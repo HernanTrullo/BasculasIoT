f55cd30ec71d6d242d5f9834a45d74627f387dcb
Hernan Dario Trullo <trullodario@unicauca.edu.co>
2025-05-31 18:45:25 -0500

    porcentaje de desviación

--- main.js
+++ main.js
@@ -81,7 +81,7 @@ ws.onmessage = (evt) => {
   const items = [
     { label: "Mejor combinación", value: `[${data.combinacion.join(", ")}]` },
     { label: "Peso objetivo", value: `${peso_objetivo} g` },
-    { label: "Peso encontrado", value: `${peso_encontrado.toFixed(2)} g` },
+    { label: "Peso encontrado", value: `${peso_encontrado} g` },
     { label: "Desviación", value: `${porcentaje_error.toFixed(2)} %` },
     { label: "Promedio pesos", value: `${promedio.toFixed(2)} g` },
     { label: "Uso de básculas", value: `${data.mejorTamano}` },
